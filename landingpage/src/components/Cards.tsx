52298df4df5f5ff907d92aa58f502793a020c18c
joaovitorss472 <180720272+joaovitorss472@users.noreply.github.com>
2026-04-01 00:04:48 -0300

    Atualização Cards.tsx

--- landingpage/src/components/Cards.tsx
+++ landingpage/src/components/Cards.tsx
@@ -1,19 +1,18 @@
-import menu from '../assets/pedidos.svg'
-import  '../styles/solution.css'
+import '../styles/solution.css'
 
 interface CardProps {
     titulo: string;
     paragrafo: string;
+    imagem: string; 
 }
 
-
-export default function Card({ titulo, paragrafo }: CardProps) {
+export default function Card({ titulo, paragrafo, imagem }: CardProps) {
     return (
         <div className="card">
-            <span>
-                <img src={menu} alt="pedido" width={64} height={64} />
+            <span className="card-image-wrapper">
+                <img src={imagem} alt={`Imagem de ${titulo}`} className="produto-img" width={64} height={64} />
             </span>
-            <div  className="divteste">
+            <div className="divteste">
                 <h3>
                     {titulo}
                 </h3>
